4c55c018b0bd7bd14ba0b6a52fed244cb415e391
William Ku <wku@kaplan.com>
2016-02-13 23:27:36 +0800

    Initial commit

---
+++ app/src/main/java/sg/com/kaplan/pdma/currencyexchangerates/MainActivity.java
@@ -0,0 +1,112 @@
+package sg.com.kaplan.pdma.currencyexchangerates;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    String url = "http://api.fixer.io/latest?base=SGD";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        //http://developer.android.com/training/volley/request.html
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                progressBar.setVisibility(View.GONE);
+                textView.setText("Response: " + response.toString());
+
+                //process response
+                try {
+                    String base = response.getString("base");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    JSONObject rates = response.getJSONObject("rates");
+
+                    Iterator<String> keys = rates.keys();
+
+                    StringBuilder sb = new StringBuilder();
+                    while(keys.hasNext()) {
+                        String currency =  keys.next();
+                        sb.append(currency.toString()+ " : " + rates.getDouble(currency) + "\n");
+                    }
+                    textView.setText(sb.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressBar.setVisibility(View.GONE);
+                //Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                textView.setText("Error: " + error.toString());
+            }
+        });
+
+        //http://stackoverflow.com/a/22169775/3839235
+        //Set a retry policy in case of SocketTimeout & ConnectionTimeout Exceptions.
+        //Volley does retry for you if you have specified the policy.
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(5000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
